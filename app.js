d2c8e78a8dd5716063bc76c5c0c8c21c013030f6
Martin Bing <bing.martin.photography@gmail.com>
2017-12-26 13:29:10 +0100

    Connect http and create url

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import * as ELEMENTS from 'elements.js';
+import { Http } from './http.service.js';
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = '';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
     const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+    const getURL = API_URL + CITY_NAME + '&units=metric&appid=' + API_KEY;
 
     if (!CITY_NAME) {
         return alert('Please enter a city name!')
     }
-
-    console.log(CITY_NAME);
+    
+    Http.fetchData(getURL)
+        .then((responseData) => {
+            console.log(responseData)
+        })
+        .catch(error => console.error(error));
 }
